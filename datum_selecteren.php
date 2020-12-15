f382b9c832333f9135111a9af8b9e6ad9b6457fb
Bram van Dijke <bvandijke03@gmail.com>
2020-12-15 16:10:23 +0100

    Reserveringssyteem zonder complete php

--- datum_selecteren.php
+++ datum_selecteren.php
@@ -87,24 +87,53 @@
 <!--Ik wil ook nog een topnav en een footer, daarnaast moet je ook nog de pagina opmaken en alles centreren-->
 
 <!--Formulier (nog niet aangepast aan de informatie die in de database moet komen, dat mag jij doen), zoek maar even op w3 voor de tags van een form ofzo.-->
-<section>
 
-<h1>Taak aanmaken</h1>
+ <header>
+    <h1>Taak aanmaken</h1>
+ </header>
+
+
+<br><br><br><br>
+
+<section>
 
 <h2>Persoonlijke details</h2>
 
 <form action="" method="post">
-    Voornaam: <br><input type="text" name="firstname"> <br><br>
-    Achternaam: <br><input type="text" name="lastname"> <br><br>
-    Postcode: <br><input type="text" name="postal"> <br><br>
-    Straatnaam: <br><input type="text" name="streetname"> <br><br>
-    Huisnummer: <br><input type="text" name="housenumber"> <br><br>
-    Woonplaats: <br><input type="text" name="city"> <br><br>
-    Emailadres: <br><input type="text" name="email"> <br><br>
-    Telefoonnummer: <br><input type="text" name="phonenumber"> <br><br>
-    Datum selecteren: <br><input type="text" id="datepicker" name="date"> <br><br>
-    <p>Beschrijving:</p>
-    <textarea rows="4" cols="50" name="comment" form="usrform">Vul hier uw tekst in...</textarea><br>
+    Naam:
+        <input type="text" name="firstname" placeholder="Voornaam">
+        <input type="text" name="lastname" placeholder="Achternaam"><br><br>
+
+    Adres:
+        <input type="text" name="streetname" placeholder="Straatnaam">
+        <input type="text" name="housenumber" placeholder="Huisnummer"><br><br>
+
+    Woonplaats:
+        <input type="text" name="city" placeholder="Stad/Dorp">
+        <input type="text" name="postal"placeholder="Postcode"> <br><br>
+
+    Contactgegevens:
+        <input type="text" name="email" placeholder="Emailadres">
+        <input type="text" name="phonenumber" placeholder="Telefoonnummer"> <br><br>
+    <hr style="width:40%;text-align:left;margin-left:0">
+<h2>Taak details</h2>
+
+    Titel:
+        <input type="text" name="title" placeholder="Titel van de taak"><br><br>
+
+    Beschrijving:
+        <textarea rows="4" cols="50" name="comment" form="usrform">Vul hier uw tekst in...</textarea><br><br>
+
+    Deadline:
+        <input type="text" id="datepicker" name="date" placeholder="dd/mm/jjjj"> <br><br>
+
+    Uurloon:
+    <input type="text" name="hourlywage" placeholder="Het uurloon van de werknemer"><br><br>
+
+    Verwachte tijdsbesteding:
+    <input type="text" name="expectedtime" placeholder="Verwachte hoeveelheid tijd"><br><br>
+
+    <hr style="width:40%;text-align:left;margin-left:0">
     <input type="submit" name="submit" value="Verzenden">
     <br>
 </form>
