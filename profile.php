0d39c239d81fb0026f7b2c2e65fd1887b6b702c3
Nils <vosnils18@icloud.com>
2021-01-07 21:08:55 +0100

    Edit profile file made

--- profile.php
+++ profile.php
@@ -1,7 +1,8 @@
 <?php
     include_once 'includes/header.php';
-    include_once 'includes/dbh.inc.php';
-    
+
+    require_once 'includes/userdata.php';
+
 ?>
 
 <html>
@@ -12,44 +13,15 @@
     <body>
         <div class="profilecontainer">
             <div class="userinformation">
-            <?php
-                $userId = $_SESSION['userid'];
-                $sql = "SELECT * FROM useraccounts WHERE user_id = $userId;";
-                $result = mysqli_query($conn, $sql);
-                $resultCheck = mysqli_num_rows($result);
-
-                if (!isset($_SESSION['username'])) {
-                    header('location: index.php');
-                    exit();
-                }
-                else {
-                    if ($resultCheck > 0) {
-                        while ($row = mysqli_fetch_assoc($result)) {
-                            echo "<p>";
-                            echo "<div class='titel'>'";
-                            echo "Gebruikersnaam: ";
-                            echo "<div>";
-                            echo $row['username'];
-                            echo "<br><br>";
-                            echo "<div class='titel'>";
-                            echo "Volledige naam: ";
-                            echo "<div>";
-                            echo $row['name'];
-                            echo "<br><br> Email: ";
-                            echo $row['email'];
-                            echo "<br><br> Adres: ";
-                            if (!isset($row['streetname']) || $row['streetname'] = '') {
-                                echo "Niet opgegeven";
-                            }
-                            else {
-                                echo $row['streetname'] . $row['house_number'];
-                            }
-                            echo "</p>";
-
-                        }
-                    }
-                }
-                ?>
+                    <?php foreach ($userInformation as $userInfo) { ?>
+                            <h4>Gebruikersnaam:</h4>
+                            <p><?= $userInfo['username']; ?></p>
+                            <h4>Volledige naam: </h4>
+                            <p><?= $userInfo['name']; ?></p>
+                            <h4>Emailadres: </h4>
+                            <p><?= $userInfo['email']; ?></p>
+                            <a href="editprofile.php?id=<?= $userInfo['user_id']; ?>">Edit</a>
+                    <?php } ?>
             </div>
         </body>
 
