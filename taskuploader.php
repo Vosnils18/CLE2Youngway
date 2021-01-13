f4bfbbf6870fce96f35d66d2c7a34bd3c9d77934
Bram van Dijke <bvandijke03@gmail.com>
2021-01-13 15:55:47 +0100

    Front-end kluspagina update

--- taskuploader.php
+++ taskuploader.php
@@ -3,7 +3,6 @@
 
 <?php
 include_once "includes/header.php"
-
 ?>
 
 <!DOCTYPE html>
@@ -14,24 +13,29 @@ include_once "includes/header.php"
 <div>
     <form action="./taskuploader.php">
         <label for="taskname">Naam van klus:</label>
-        <input type="text" id="taskname" name="taskname" placeholder="klusnaam...">
+        <input type="text" id="taskname" name="taskname" placeholder="klusnaam..." required>
 
         <label for="hourlywage">Uurloon:</label>
-        <input type="text" id="hourlywage" name="hourlywage" placeholder="€...">
+        <input type="text" id="hourlywage" name="hourlywage" placeholder="€..." required>
 
         <label for="taskcat">Categorie van klus:</label>
-        <select id="taskcat" name="taskcat">
+        <select id="taskcat" name="taskcat" required>
             <option value="standardopt">Kies hier de categorie van uw klus...</option>
             <option value="schilderen">Schilderen</option>
             <option value="elektronica">Elektronica</option>
             <option value="cleaning">Schoonmaken</option>
             <option value="softwarehelp">Software</option>
             <option value="garden">Tuin</option>
+            <option value="overig">Overig</option>
         </select>
+
         <label for="deadlineselect">Datum selecteren:</label>
         <input type="date" id="start" name="trip-start"
                value="2021-01-01"
-               min="2021-01-01" max="2030-12-31">
+               min="2021-01-01" max="2030-12-31" required>
+
+        <label for="taskdescription">Beschrijving van taak </label>
+        <input style="padding-bottom: 4% " type="text" placeholder="informatie klus..." name="temp1" required>
         <input type="submit" value="Submit">
     </form>
 </div>
