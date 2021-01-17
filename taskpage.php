9a7f7e4a27f4987bb918620b2b0e2201230aba47
Bram van Dijke <bvandijke03@gmail.com>
2021-01-17 21:56:09 +0100

    Taskpage aangemaakt

---
+++ taskpage.php
@@ -0,0 +1,21 @@
+<?php
+include_once "includes/header.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" type="text/css" href="style/taskpagestyle.css"/>
+<br>
+<div class="backgroundpage">
+    <br>
+<div class="tempor1">
+    <div class="titletask">Titel van klus</div>
+    <div class="deadlinedate">Deadline: 1-1-1111</div>
+    <div class="categorytask">Elektronica</div>
+    <div class="descriptiontask">
+        <h2>Beschrijving</h2>
+        <p>ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+    </div>
+
+</div>
+</div>
