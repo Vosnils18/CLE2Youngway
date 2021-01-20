e6cf97889a711fd5577a651dcd378fd4f8393acb
Nils <vosnils18@icloud.com>
2021-01-20 12:42:46 +0100

    pp

--- idc.php
+++ idc.php
@@ -8,57 +8,54 @@ $result = mysqli_query($conn, $query)
 or die ("Connection failed: " . mysqli_connect_error());
 
 //Loop through the result to create a custom array
-$userInformation = [];
+$appointments = [];
 while ($row = mysqli_fetch_assoc($result)) {
-    $userInformation[] = $row;
+    $appointments[] = $row;
 }
 
 //Close connection
 mysqli_close($conn);
 ?>
 
-<?php foreach ($userInformation as $userInfo) { ?>
-    <h4>Gebruikersnaam:</h4>
-    <item><?= $userInfo['username']; ?></item>
-    <h4>Volledige naam: </h4>
-    <p><?= $userInfo['name']; ?></p>
-    <h4>Emailadres: </h4>
-    <p><?= $userInfo['email']; ?></p>
-    <h4>Adres: </h4>
-    <p><?php
+
+<h4>coole tabel</h4>
+
+<table>
+<?php foreach ($appointments as $appointments) { ?>
+    <tr>
+        <th>Gebruikersnaam: </th>
+        <th>Volledige naam: </th>
+        <th>Emailadres: </th>
+        <th>Adres: </th>
+        <th>Telefoonnummer: </th>
+        <th>Judgement: </th>
+    </tr>
+    <td><?= $appointments['username']; ?></td>
+    <td><?= $appointments['name']; ?></td>
+    <td><?= $appointments['email']; ?></td>
+    <td><?php
         if (!isset($userInfo['streetname']) || !isset($userInfo['house_number'])) {
             echo "niet opgegeven";
         }
         else {
             echo $userInfo['streetname'] . $userInfo['house_number'];
         }
-        ?></p>
-    <h4>telefoonnummer: </h4>
-    <p><?php
+        ?></td>
+    <td><?php
         if (!isset($userInfo['phone'])) {
             echo "niet opgegeven";
         }
         else {
             echo $userInfo['phone'];
         }
-        ?></p>
-    <a href="anderepagina.php">yes</a>
-    <a href="deleteaccount.php?id=<?= $userInfo['user_id'] ?>">Delete</a>
+        ?></td>
+    <td><a href="yes.php">yes</a></td>
+    <td><a href="no.php">no</a></td>
 <?php } ?>
+</table>
 
 
 
-
-
-<html>
-<form action="welcome.php" method="post">
-    <textarea>
-    </textarea>
-
-    <input type="submit" name="submit">
-</form>
-
-</html>
 
 
 
