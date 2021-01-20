8b47fc308d550eb21a3e5b2fe10ae74b4997e217
Bram van Dijke <bvandijke03@gmail.com>
2021-01-20 12:42:01 +0100

    Homepage + footer adjustment

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     include_once "includes/header.php"
 
 ?>
-
+<html>
 <link rel="stylesheet" href="style/errorstyle.css">
     <link rel="stylesheet" href="style/homepagestyle.css" type="text/css">
     <title>Youngway</title>
@@ -38,26 +38,18 @@ if (isset($_GET["event"])) {
         </div>
         <form class="example" action="taskpage.php" method="post">
             <input type="text" placeholder="Zoeken.." name="search2">
-            <button type="submit">zoek</button>
         </form>
-
     </div>
 </div>
 
-<div class="wayitworks">
-    <p>Hoe werkt het?</p>
-</div>
+<div class="quickguideye">
+    <div class="square1"><img src="images/guideimage1.png"></div>
+    <div class="square2"><img src="images/guideimage2.png"></div>
+    <div class="square3"><img src="images/guideimage3.png"></div>
+    <div class="square4"><h2>Stap 1: Oriëntatie</h2><p>U bent op zoek naar iemand die uw klus kan vervullen.  Youngway biedt veel verschillende diensten aan dus wij kunnen u vast helpen!</p></div>
+    <div class="square5"><h2>Stap 2: Contact</h2><p>Nadat u besluit te kiezen voor youngway nemen we contact met u op. Hier vragen wij naar eigenschappen van de klus, persoonlijke gevens en leren we u kennen.</p> </div>
+    <div class="square6"><h2>Stap 3: Afronding</h2><p>Youngway komt bij u langs om de klus te voltooien. Er wordt achteraf en factuur verstuurd of indien van te voren afgesproken een offerte nageleefd.</p> </div>
 
-<div class="quickguide">
-    <img src="images/placeholderimg.png" style="width:200px;height:200px;">
-    <img src="images/placeholderimg.png" style="width:200px;height:200px;">
-    <img src="images/placeholderimg.png" style="width:200px;height:200px;">
-</div>
-
-<div class="quickguide2">
-    <p>Stap 1</p>
-    <p>Stap 2</p>
-    <p>Stap 3</p>
 </div>
 </body>
 </html>
